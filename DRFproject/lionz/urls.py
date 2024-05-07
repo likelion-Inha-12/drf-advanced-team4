575a02cd3c98813cd6c7834461ffeff5b9a980b6
kaswhy <sxshx818@naver.com>
2024-05-08 01:25:09 +0900

    fix: 4, 5, 6번 과제 수정

--- DRFproject/lionz/urls.py
+++ DRFproject/lionz/urls.py
@@ -7,9 +7,7 @@ urlpatterns = [
     path('assignments', AssignmentCreateAPIView.as_view(), name='assignment-create'),
     path('submissions', SubmissionCreateAPIView.as_view(), name='submission-create'),
     path('assignments/all', AssignmentListAPIView.as_view(), name='assignment-list'),
-    path('assignments/<int:pk>', AssignmentRetrieveAPIView.as_view(), name='assignment-detail'),
-    path('assignments/update/<int:pk>', updateAssignment, name='assignment-update'),
-    path('assignments/delete/<int:pk>', AssignmentDeleteView.as_view(), name='assignment-delete'),
+    path('assignments/<int:pk>', AssignmentRetrieveUpdateDestroyAPIView.as_view(), name='assignment-detail'),
     path('assignments/part', AssignmentPartAPIView.as_view(), name='assignment-part-list'),
     path('assignments/category', AssignmentCategoryAPIView.as_view(), name='assignment-category-list'),
 ]
